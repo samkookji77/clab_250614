f5bcaf8585932fb59957a3c9feaf037f2b6ec728
samkookji77 <samkookji77@gmail.com>
2025-06-11 13:18:44 +0900

    modify cde

--- srl-telemetry-lab-4c8g/traffic.sh
+++ srl-telemetry-lab-4c8g/traffic.sh
@@ -6,56 +6,59 @@
 
 set -eu
 
-# Function to start traffic
-start_traffic() {
-    case $1 in
-        "1-2")
-            sudo docker exec client1 bash /config/iperf.sh
-            ;;
-        "2-1")
-            sudo docker exec client2 bash /config/iperf.sh
-            ;;
-        "all")
-            sudo docker exec client1 bash /config/iperf.sh
-            sudo docker exec client2 bash /config/iperf.sh
-            ;;
-        *)
-            echo "Invalid traffic pattern. Use 1-2, 2-1, or all"
-            exit 1
-            ;;
-    esac
+startTraffic1-2() {
+    echo "starting traffic between clients 1 and 2"
+    sudo docker exec client2 bash /config/iperf.sh
 }
 
-# Function to stop traffic
-stop_traffic() {
-    case $1 in
-        "1-2")
-            sudo docker exec client1 pkill iperf3
-            ;;
-        "2-1")
-            sudo docker exec client2 pkill iperf3
-            ;;
-        "all")
-            sudo docker exec client1 pkill iperf3
-            sudo docker exec client2 pkill iperf3
-            ;;
-        *)
-            echo "Invalid traffic pattern. Use 1-2, 2-1, or all"
-            exit 1
-            ;;
-    esac
+#startTraffic1-3() {
+#    echo "starting traffic between clients 1 and 3"
+#    sudo docker exec client3 bash /config/iperf.sh
+#}
+
+startAll() {
+    echo "starting traffic on all clients"
+    sudo docker exec client2 bash /config/iperf.sh
+   # sudo docker exec client3 bash /config/iperf.sh
 }
 
-# Main script
-case $1 in
-    "start")
-        start_traffic $2
-        ;;
-    "stop")
-        stop_traffic $2
-        ;;
-    *)
-        echo "Usage: $0 {start|stop} {1-2|2-1|all}"
-        exit 1
-        ;;
-esac
+stopTraffic1-2() {
+    echo "stopping traffic between clients 1 and 2"
+    sudo docker exec client2 pkill iperf3
+}
+
+#stopTraffic1-3() {
+#    echo "stopping traffic between clients 1 and 3"
+#    sudo docker exec client3 pkill iperf3
+#}
+
+stopAll() {
+    echo "stopping all traffic"
+    sudo docker exec client2 pkill iperf3
+   # sudo docker exec client3 pkill iperf3
+}
+
+# start traffic
+if [ $1 == "start" ]; then
+    if [ $2 == "1-2" ]; then
+        startTraffic1-2
+    fi
+    #if [ $2 == "1-3" ]; then
+    #    startTraffic1-3
+    #fi
+    if [ $2 == "all" ]; then
+        startAll
+    fi
+fi
+
+if [ $1 == "stop" ]; then
+    if [ $2 == "1-2" ]; then
+        stopTraffic1-2
+    fi
+    #if [ $2 == "1-3" ]; then
+    #    stopTraffic1-3
+    #fi
+    if [ $2 == "all" ]; then
+        stopAll
+    fi
+fi
